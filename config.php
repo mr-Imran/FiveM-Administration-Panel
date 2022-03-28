0be3406d8aa5f8e3ccff0ab7c4f371e3a276dd69
MR Imrann <39611124+mr-Imran@users.noreply.github.com>
2022-03-28 15:47:29 +0600

    c

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 	
 	#region Database Settings
 	// DATABASE SETTINGS
-	$GLOBALS['mysql_host'] = "";								// MySQL Host
-	$GLOBALS['mysql_user'] = "";								// MySQL User
-	$GLOBALS['mysql_pass'] = "";								// MySQL Password
-	$GLOBALS['mysql_db'] = "";									// MySQL Database
+	$GLOBALS['mysql_host'] = "containers-us-west-33.railway.app";								// MySQL Host
+	$GLOBALS['mysql_user'] = "root";								// MySQL User
+	$GLOBALS['mysql_pass'] = "DdtkltE7FlOqyKK7nKdt";								// MySQL Password
+	$GLOBALS['mysql_db'] = "railway";									// MySQL Database
 	#endregion
 	
 	// SITE SETTINGS
@@ -15,3 +15,4 @@
 	date_default_timezone_set('America/New_York');						// Timezone (http://php.net/manual/en/timezones.php)
 
 ?>
+
